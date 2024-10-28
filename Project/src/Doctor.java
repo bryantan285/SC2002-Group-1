fb8aef379e3df74ff33392e78e59412e8d938b55
vcdice-69 <160406447+vcdice-69@users.noreply.github.com>
2024-10-28 22:55:02 +0800

    Update Doctor.java

--- Project/src/Doctor.java
+++ Project/src/Doctor.java
@@ -1,3 +1,25 @@
-public class Doctor {
-    
+public class Doctor extends User{
+    private ArrayList<Appointment> upcomingAppointments;
+
+    public Doctor(Role userRole, String userId, String userPassword){
+        super(Role.DOCTOR, userId, userPassword);
+    }
+
+
+    public void viewSchedule(){
+        for (Appointment appt : upcomingAppointments){
+            System.out.printf("You have an appointment with %s at %tF %<tT%n", appt.getPatientId(), appt.getApptDate());
+        }
+    }
+
+    public void setAvailabilityForAppts(Date )[
+
+    ]
+
+    public void acceptDeclineApptRequest(Appointment appt, boolean apptStatus){
+        appt.setStatus(apptStatus);
+    }
+    public void recordApptOutcome(Appointment appt, String doctorsNotes){
+        appt.setApptOutcome(doctorsNotes);
+    }
 }
