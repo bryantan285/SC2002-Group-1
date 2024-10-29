b739881e7016c1a0b779dc8ea1494016cbe289a4
vcdice-69 <160406447+vcdice-69@users.noreply.github.com>
2024-10-29 14:22:05 +0800

    Update User.java

--- Project/src/User.java
+++ Project/src/User.java
@@ -1,29 +1,34 @@
-public abstract class User {
-    enum Role {PATIENT, DOCTOR, PHARMACIST, ADMINISTRATOR}
+import java.util.*;
 
-    private Role role;
+
+public abstract class User {
+
+    private boolean isPatient;
     private String id;
+    private String name;
     private String password = "password"; //default password
     private boolean gender; //true = male; false = female
 
-
-    //Constructor function to create a new User obj
-    public User(Role userRole, String userId, String userPassword, boolean gender){
-        this.role = userRole;
+    //password = "password" initially
+    public User(boolean isPatient, String userId, String name, boolean gender){
+        this.isPatient = isPatient;
         this.id = userId;
-        this.password = userPassword;
+        this.name = name;
         this.gender = gender;
     }
 
     //Getters
+    public boolean getIsPatient() {
+        return this.isPatient;
+    }
     public String getId() {
         return this.id;
     }
+    public String getName() {
+        return this.name;
+    }
     public String getPassword() {
         return this.password;
-    }
-    public Role getRole() {
-        return this.role;
     }
     public boolean getGender() {
         return this.gender;
@@ -31,19 +36,21 @@ public abstract class User {
 
     
     //Setters
+    public void setIsPatient(boolean isPatient) {
+        this.isPatient = isPatient;
+    }
     public void setId(String id) {
         this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
     }
     public void setPassword(String password) {
         this.password = password;
     }
-    public void setRole(Role role) {
-        this.role = role;
-    }
     public void setGender(boolean gender) {
         this.gender = gender;
     }
-
 
     //function definitions
     public boolean login(String inputId, String inputPassword){
