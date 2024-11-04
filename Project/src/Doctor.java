0c2b0cdf45f2758caf39903bc0705e097f17e74b
vcdice-69 <160406447+vcdice-69@users.noreply.github.com>
2024-11-04 22:54:55 +0800

    waiting for patient definition now

--- Project/src/Doctor.java
+++ Project/src/Doctor.java
@@ -1,25 +1,61 @@
-public class Doctor extends User{
-    private ArrayList<Appointment> upcomingAppointments;
+import java.util.*;
+import java.time.*;
 
-    public Doctor(Role userRole, String userId, String userPassword){
-        super(Role.DOCTOR, userId, userPassword);
+public class Doctor extends HospitalStaff{
+    private ArrayList<Appointment> upcomingAppointments;
+    private ArrayList<UnavailableDates> unavailableDates;
+
+    public Doctor(boolean isPatient, String userId, String name, boolean gender, int age){
+        super(isPatient, userId, name, gender, HospitalStaff.Role.DOCTOR, age);
+    }
+
+    public void viewSchedule(){
+        for (UnavailableDates dates : unavailableDates){
+            System.out.printf("You are unavailable from %s to %s%n", dates.getStartDateTime(), dates.getEndDateTime());
+        }
+        System.out.println();
+        viewUpcomingAppts();
     }
 
 
-    public void viewSchedule(){
+    public void viewUpcomingAppts(){
         for (Appointment appt : upcomingAppointments){
-            System.out.printf("You have an appointment with %s at %tF %<tT%n", appt.getPatientId(), appt.getApptDate());
+            System.out.printf("You have an appointment with %s at %tF %<tT%n", appt.getPatientId(), appt.getApptDateTime());
         }
     }
 
-    public void setAvailabilityForAppts(Date )[
-
-    ]
-
-    public void acceptDeclineApptRequest(Appointment appt, boolean apptStatus){
-        appt.setStatus(apptStatus);
+    //
+    public void setAvailabilityForAppts(LocalDateTime startTime, LocalDateTime endTime){
+        UnavailableDates duration = new UnavailableDates(startTime, endTime);
+        if (duration.isValidDuration()) {
+            unavailableDates.add(duration);
+            System.out.println("Unavailability successfully set");
+        } else 
+            System.out.println("Could not set unavability");
     }
-    public void recordApptOutcome(Appointment appt, String doctorsNotes){
-        appt.setApptOutcome(doctorsNotes);
+
+    public void acceptDeclineApptRequest(Appointment appt, boolean isAccepted){
+        if (isAccepted) {
+            appt.setStatus(Appointment.Status.CONFIRMED);
+            System.out.println("Appointment sucessfully confirmed.");
+        }
+        else {
+            appt.setStatus(Appointment.Status.REJECTED);
+            System.out.println("Appointment sucessfully declined.");
+        }
+    }
+
+    public void recordApptOutcome(Appointment appt, String apptOutcome){
+        appt.setApptOutcome(apptOutcome);
+        appt.setStatus(Appointment.Status.COMPLETED);
+    }
+
+    public void viewPatientMedicalRecord(Patient patient) {
+        
+    }
+
+    // Update medical record by adding a new diagnosis, prescription, or treatment
+    public void updatePatientMedicalRecord(Patient patient, String diagnosis, String prescription, String treatment) {
+        
     }
 }
